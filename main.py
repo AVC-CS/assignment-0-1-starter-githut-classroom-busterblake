ee275f9d8effddb325bba2183f3ece5311f7a7a4
busterblake <33817212+busterblake@users.noreply.github.com>
2023-08-18 17:17:14 -0700

    main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 def main():
 
     print('Hello World')
-    print('CS122: Programming and Algorithms in Python')
+    print('CS122: Programming and Algorithms in Python') 
 
 
 if __name__ == '__main__':
